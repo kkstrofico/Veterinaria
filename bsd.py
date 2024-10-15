3bd766dca3965ab6737827e82da9a10596daf7e3
Junior Herrera <herrerajunior715@gnmail.com>
2024-10-15 14:39:11 -0500

    Se creo una clase mascota que tiene como parametros los atributos de la mascotas, ademas en el modulo bsd se crearon las listas generales y de los id de los actores

---
+++ bsd.py
@@ -0,0 +1,9 @@
+# modulo que contiene las listas generales
+#listas que almacenaran los ides de la mascota, propietario, veterinario
+ides_veterinarios=[]
+ides_propietarios=[]
+ides_mascotas=[]
+#listas generales que almacenaran las informaciones de los actores
+lista_veterinarios =[]
+lista_mascota=[]
+lista_propietario =[]
