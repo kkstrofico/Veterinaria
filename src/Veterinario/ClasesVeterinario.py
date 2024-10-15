d187372a9d1bcfa709c134a888fea2f0935d7525
Sena <SENA@CLAAPRCRNFSD069.FormacionSena.RED>
2024-10-15 15:14:09 -0500

    Se Creo el metodo del veterinario que se encarga de guardar los datos del veterinario a su respectiva lista

--- src/Veterinario/ClasesVeterinario.py
+++ src/Veterinario/ClasesVeterinario.py
@@ -1,9 +1,23 @@
+import bsd
 class Veterinario:#Clase Veterinario
-    def __init__(self,id,nombre,apellidos,direccion,telefono,tarjeta_profesional):#Todos sus atributos
+    def __init__(self,id,nombres,apellidos,direccion,telefono,tarjeta):#Todos sus atributos
         self.id = id
-        self.nombre = nombre
-        self.apellido = apellidos
+        self.nombres = nombres
+        self.apellidos = apellidos
         self.direccion = direccion
         self.telfono = telefono
-        self.tarjeta = tarjeta_profesional
+        self.tarjeta = tarjeta
+        pass
+    def registrar_veterinario(self):
+        diccionario_veterinario = {
+            "id":self.id,
+            "nombres":self.nombres,
+            "apellidos":self.apellidos,
+            "direccion":self.direccion,
+            "telefono":self.telfono,
+            "tarjeta profesional":self.tarjeta
+        }
+        bsd.ides_veterinarios.append(self.id)#Se guardardo el id del veterinario a la lista respectiva
+        bsd.lista_veterinarios.append(diccionario_veterinario) #Se subio a la 
+    def tarjeta_profesional(self):#MEtodo que solicita la tarjeta profesional
         pass
