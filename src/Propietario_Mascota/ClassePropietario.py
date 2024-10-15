dbceaf7a834d774251f722abca3f97907c0e70b0
Camilo Andres <ospinavillacamiloandres@gmail.com>
2024-10-15 15:22:04 -0500

    se creo un metodo para almacenar los datos del propietario en un diccionario.ademas se añadio a la lista ides_propietarios el id y a lista_propietario se añade el diccionario con los datos del propietario.

--- src/Propietario_Mascota/ClassePropietario.py
+++ src/Propietario_Mascota/ClassePropietario.py
@@ -2,7 +2,7 @@ import bsd
 class Propietario: #clase propietario que contiene la informacion del propietario que se va a pedir
     def __init__(self,id="", nombres="", apellidos="", direccion="", telefono="", correo="", mascota=""): #sus atributos
         self.id = id
-        self.nombres = nombres|
+        self.nombres = nombres
         self.apellidos = apellidos
         self.direccion = direccion
         self.telefono = telefono
