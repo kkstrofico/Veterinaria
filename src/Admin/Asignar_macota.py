7a2c1a49d5387d517bca4e243d0e352bd198b544
Junior Herrera <herrerajunior715@gnmail.com>
2024-10-15 16:33:16 -0500

    Se agrego un diagrama de flujo que representa el flujo de agregar una mascota a un veterinario, ademas se creo un modulo el cual tendra la funcion (Asignar_mascota) la cual se encarga de asignar una mascota a un veterinario

---
+++ src/Admin/Asignar_macota.py
@@ -0,0 +1,27 @@
+import bsd # se importa las listas que contienen todd la información
+def asignar(): # funcion que se encargara de asignar un veterinario a una mascota, y una mascota a un veterinario
+    while True:
+        id_veterinario = input("Ingresa el id veterinario: ") # solicitar id del veterinario
+        if len(id_veterinario) !=10: # excepcion
+            print("Error, ingresa 10 digitos")
+        elif id_veterinario.isnumeric()== False: # Si el id_veterinario no es numerico
+            print("Error, ingresa numeros")
+        elif id_veterinario not in bsd.ides_veterinarios: # si el id no se encuentra en la lista ides_veterinarios
+            print("Error, ese id no se encuentra registrado.")
+        else: # finalizar bucle
+            break
+    for i in bsd.lista_veterinarios: # recorrer lista que contiene los diccionarios de todos los veterinarios
+        if i["id"]== id_veterinario: # se compara si el id que se encuentra en este es igual al id que fue ingresado
+            print("\n INGRESA EL ID DE LA MASCOTA\n")
+            while True:
+                id_mascota= input("Ingresa el id de la mascota: ")
+                if len(id_mascota) != 4:
+                    print("El id de la mascota es de 4 digitos")
+                elif id_mascota not in bsd.ides_mascotas:
+                    print("Error, ese id no se encuentra")
+                    # continuar JUNIOR
+                
+        
+            
+    
+    
