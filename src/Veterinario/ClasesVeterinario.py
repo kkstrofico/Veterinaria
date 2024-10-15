7729263e9f60e87bbf428a46e42901b07cc49212
unknown <SENA@CLAAPRCRNFSD069.FormacionSena.RED>
2024-10-15 14:40:52 -0500

    Se creo la classe veterinario con sus respectivos atributos

---
+++ src/Veterinario/ClasesVeterinario.py
@@ -0,0 +1,9 @@
+class Veterinario:#Clase Veterinario
+    def __init__(self,id,nombre,apellidos,direccion,telefono,tarjeta_profesional):#Todos sus atributos
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellidos
+        self.direccion = direccion
+        self.telfono = telefono
+        self.tarjeta = tarjeta_profesional
+        pass
