0b8f28e82a9896f8f3df151bddc6fcf7952928e0
Junior Herrera <herrerajunior715@gnmail.com>
2024-10-16 13:20:46 -0500

    Se modifico la clase Admin, especialmente el metodo que agrega un veterinario a una mascota

--- src/ClaseAdmin.py
+++ src/ClaseAdmin.py
@@ -1,3 +1,5 @@
+from itertools import chain # este modulo permite realizar la manipulacion de datos de manera rapida
+
 import bsd
 import ClasesVeterinario
 class admin:
@@ -19,42 +21,87 @@ class admin:
                 for k in bsd.lista_veterinarios:
                     mascotas_asignadas.append(k["Mascotas"])
                 mascotas_asignadas_mod=list(chain.from_iterable(mascotas_asignadas)) # esto permite convertir varias listas en una sola
-                
-                mascotas_sin_veterinario=[] # id de mascotas que no tienen veterinario
+
                 for j in bsd.lista_mascota: # un for que iterara sobre las mascotas y mostrara las que no tienen veterinario
+                    mascotas_sin_veterinario=[] # id de mascotas que no tienen veterinario
                     if j["Veterinario"]=="": # si en el diccionario de las mascotas, en la clave Veterinario tenga un valor, en caso    de que no, se le asignara el id del veterinario a essta mascota
                         mascotas_sin_veterinario.append(j["id"])
-                print(f"\n MASCOTAS SIN VETERINARIO\n {mascotas_sin_veterinario}")
-                while True:
-                    id_mascota= input("Ingresa el id de la mascota: ") # se solicita el numero de id de la mascota
-                    if len(id_mascota) != 4:
-                        print("El id de la mascota es de 4 digitos")
-                    elif id_mascota not in bsd.ides_mascotas: # si el numero de ficha no se encuentra en la lista ides_mascotas
-                        print("Error, ese id no se encuentra")
-                    elif id_mascota.isnumeric()==False:
-                        print("Error, ingresa numeros")
-                    elif id_mascota in i["Mascotas"]: # si el id de la mascota ya esta asignada al veterinario
-                        print("Este veterinario ya tiene esa mascota")
-                    elif id_mascota in mascotas_asignadas_mod:
-                        print("Esa mascota ya le pertenece a otro veterinario")
-                    else:
-                        break
-                i["Mascotas"].append(id_mascota) # se agrega el id de la mascota a la informacion del veterinario
-                j["Veterinario"]=id_veterinario # se agrega el id del veterinario a la informacion de la mascota
-                print(f"\n  LISTA VETERINARIO\n{i}")
-                print(f"\n  LISTA MASCOTAS\n{j}")
-    
-    
+                        print(f"\n MASCOTAS SIN VETERINARIO\n {mascotas_sin_veterinario[0]}")
+                        while True:
+                            id_mascota= input("Ingresa el id de la mascota: ") # se solicita el numero de id de la mascota
+                            if len(id_mascota) != 4:
+                                print("El id de la mascota es de 4 digitos")
+                            elif id_mascota not in bsd.ides_mascotas: # si el numero de ficha no se encuentra en la listaides_mascotas
+                                print("Error, ese id no se encuentra")
+                            elif id_mascota.isnumeric()==False:
+                                print("Error, ingresa numeros")
+                            elif id_mascota in i["Mascotas"]: # si el id de la mascota ya esta asignada al veterinario
+                                print("Este veterinario ya tiene esa mascota")
+                            elif id_mascota in mascotas_asignadas_mod:
+                                print("Esa mascota ya le pertenece a otro veterinario")
+                            else:
+                                break
+                        i["Mascotas"].append(id_mascota) # se agrega el id de la mascota a la informacion del veterinario
+                        j["Veterinario"]=id_veterinario # se agrega el id del veterinario a la informacion de la mascota
+                        print(f"\n  LISTA VETERINARIO\n{i}")
+                        print(f"\n  LISTA MASCOTAS\n{j}")
+                        while True:
+                            agregar_mas= input("\nDesea agregar mascota a otro veterinario?\n ").lower().strip()
+                            if agregar_mas != "si" and agregar_mas != "no":
+                                print("Error, ingresa si/no")
+                            else:
+                                break
+                        if agregar_mas =="si": # si dice que si 
+                            if mascotas_sin_veterinario ==[]: # si la lista se encuentra vacia, significa que no hay mascotas por sin veterinario
+                                print("No se encuentras mascotas sin veterinario")
+                            else:  # de lo contrario se retorna de nuevo la funcion asignar
+                                return admin.asignar()
+                        else:
+                            break # y si dice que no, finaliza el bucle
+
+
+
                 #OPCION PARA CAMBIAR EL VETERINARIO DE UNA MASCOTA QUE YA TIENE VETERINARIO
                 print("""\n DESEA MODIFICAR UN VETERINARIO DE LA MASCOTA QUE YA TIENE UN VETERINARIO ASIGNADO\n
-                    1. SI
-                    2.NO""")
+                    1. SI / 2. NO""")
                 while True:
-                    seleccion = input("").lowe()
+                    seleccion = input("").lower() #solicitar si desea cambiar el veterinario de una mascota
                     if seleccion!= "si" and seleccion !="no":
                         print("error, ingresa si/no")
                     else:
                         break
+                if seleccion =="si":
+                    while True:
+                        id_mascota_mod = input("Ingresa el id de la mascota: ") # solicitar el id de la mascota a la que se le modificara el veterinario
+                        if len(id_mascota_mod) != 4:
+                            print("Error, ingresa 4 digitos")
+                        elif id_mascota not in bsd.ides_mascotas:
+                            print("Error, ese id de mascota no se encuentra registrado")
+                        else:
+                            break
+                    for m in bsd.lista_mascota: # recorrer la lista de las mascotas para cambiar su veterinario
+                        if m["id"]== id_mascota_mod: # si el id es igual al que se encuentra en el diccionario
+                            while True:
+                                id_veterinario=input("Ingresa el id del veterinario que asignaras: ")
+                                if id_veterinario not in bsd.ides_veterinarios:
+                                    print("Error, ese id de veterinario no se encuentra registrado")
+                                elif len(id_veterinario) != 10:
+                                    print("Error, el ingresa 10 digitos")
+                                else:
+                                    break
+                            # Asignar veterinario a mascota
+                            m["Veterinario"]=id_veterinario
+                    for v in bsd.lista_veterinarios: # Redcorrer la lista de los veterinarios para agregarle la mascota
+                        if v["id"] ==id_veterinario: # si el id del diccionario es igual al id_veterinario ingresado
+                            v["Mascotas"].append(id_mascota) # Agregar mascota a la lista mascotas del veterinario
+                    print(bsd.lista_mascota)
+                    print("")
+                    print(bsd.lista_veterinarios)
+                        
+                        
+                                    
+                            
+                        
                     
     def registrar_veterinario():
         while True: 
@@ -92,4 +139,4 @@ class admin:
                 print("\n\tERROR: dato ingresado no valido")
         tarjeta = ClasesVeterinario.Veterinario.tarjeta_profesional(None) #se llama a la funcion tarjeta profesional para agregar lo de la tarjeta profesional
         ClasesVeterinario.Veterinario(id,nombre,apellido,direccion,telefono,tarjeta) #se llama a la funcion clasesVeterinario para que se le asignen los datos que fueron ingresados
-admin.registrar_veterinario()
+admin.asignar()
